2932b8883685cabaf639c3a7258058d72af9e338
Al-amin Ahmed <alaacodes@gmail.com>
2022-10-22 05:00:31 +0100

    Update print_rot13.c

--- print_rot13.c
+++ print_rot13.c
@@ -12,30 +12,28 @@ int print_R(va_list R)
 	char *str;
 	unsigned int i, j;
 	int count = 0;
-	char in[] =
-	"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-		char out[] =
-	"NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	char in[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char out[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
-		str = va_arg(R, char *);
-		if (str == NULL)
-			str = "(ahyy)";
-		for (i = 0; str[i]; i++)
+	str = va_arg(R, char *);
+	if (str == NULL)
+		str = "(ahyy)";
+	for (i = 0; str[i]; i++)
+	{
+		for (j = 0; in[j]; j++)
 		{
-			for (j = 0; in[j]; j++)
+			if (in[j] == str[i])
 			{
-				if (in[j] == str[i])
-				{
-					_putchar(out[j]);
-					count++;
-					break;
-				}
-			}
-			if (!in[j])
-			{
-				_putchar(str[i]);
+				_putchar(out[j]);
 				count++;
+				break;
 			}
 		}
-		return (count);
+		if (!in[j])
+		{
+			_putchar(str[i]);
+			count++;
+		}
+	}
+	return (count);
 }
